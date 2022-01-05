acfe6177a7edfe28789f3097c41a464980a3eba3
Jay Mistry <jayu412@gmail.com>
2022-01-05 12:12:38 +0530

    Add project files.

---
+++ SampleGrpcClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleGrpcClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string serviceEndpoint = "https://localhost:5001";
+            var channel = GrpcChannel.ForAddress(serviceEndpoint);
+            var client = new Greeter.GreeterClient(channel);
+
+            HelloRequest helloRequest = new HelloRequest
+            {
+                Name = "Jay"
+            };
+            var response = await client.SayHelloAsync(helloRequest);
+
+            Console.WriteLine($"Response obtained from the service {response.Message}");
+        }
+    }
+}
